fafd2400e2058f5a7694ff01aa0fb123521a955c
Viktoriia <vikahomeacc@gmail.com>
2024-10-18 16:46:37 +0300

    first

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+    while(true){
+        
+        Console.WriteLine("new frame");
+        var k=Console.ReadLine();
+
+        if(k=="q"){
+            break;
+        }
+    }
+         
+         var result = Console.ReadLine();
+        Console.WriteLine(result);
+
+    }
+}
